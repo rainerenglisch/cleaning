f6c86b01c7a4504060a350524a6b6832de528f51
rainerenglisch <rainer.englisch@gmail.com>
2014-09-05 11:22:13 +0200

    yup

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,11 @@ feature$label <- tolower(feature$label)
 # identify the indeces of all feature labels which contain
 # mean or std
 featureMeanStd <- grep("mean|std",feature$label)
+featureMeanStdLabel <- feature$label[featureMeanStd]
+#newFeatureMeanStdLabel <- featureMeanStdLabel 
+#Jerk of the body on the z axiz
+#newFeatureMeanStdLabel[grep("mean",feature$label)] <- "Mean of"
+#newFeatureMeanStdLabel[grep("std",feature$label)] <- "Standard deviation of"
 
 # load the table of activity codes and labels into memory
 activity <- read.table("./UCI HAR Dataset/activity_labels.txt", sep=" ", col.names=c("code","label"))
@@ -12,21 +17,24 @@ activity <- read.table("./UCI HAR Dataset/activity_labels.txt", sep=" ", col.nam
 activity$label <- c("Walking","Walking Upstairs","Walking Downstairs","Sitting","Standing","Laying")
 
 # load the subjects corresponding to the observation into memory
-subject <- rbind(read.table("./UCI HAR Dataset/train/subject_train.txt", sep=" ", col.names=c("subject")),#, nrows=100),
-                 read.table("./UCI HAR Dataset/test/subject_test.txt", sep=" ", col.names=c("subject")))#, nrows=100))
+subject <- rbind(read.table("./UCI HAR Dataset/train/subject_train.txt", sep=" ", col.names=c("subject"), nrows=100),
+                 read.table("./UCI HAR Dataset/test/subject_test.txt", sep=" ", col.names=c("subject"), nrows=100))
 
 # load all feature values of all observations into memory
 # having the feature names as column names
-featureObservations <- rbind(read.table("./UCI HAR Dataset/train/X_train.txt", header=FALSE, col.names=c(feature$label)),#, nrows=100),
-                             read.table("./UCI HAR Dataset/test/X_test.txt", header=FALSE, col.names=c(feature$label)))#, nrows=100))
+featureObservations <- rbind(read.table("./UCI HAR Dataset/train/X_train.txt", header=FALSE, col.names=c(feature$label), nrows=100),
+                             read.table("./UCI HAR Dataset/test/X_test.txt", header=FALSE, col.names=c(feature$label), nrows=100))
 
 # load the activities of all observations into memory
-activityObservation <- rbind(read.table("./UCI HAR Dataset/train/y_train.txt", sep=" ", col.names=c("code")), #, nrows=100),
-                             read.table("./UCI HAR Dataset/test/y_test.txt", sep=" ", col.names=c("code")))#, nrows=100))
+activityObservation <- rbind(read.table("./UCI HAR Dataset/train/y_train.txt", sep=" ", col.names=c("code"), nrows=100),
+                             read.table("./UCI HAR Dataset/test/y_test.txt", sep=" ", col.names=c("code"), nrows=100))
 
 # create a dataframe containing all subject, activity label and 
 # features calculating mean or standard deviation of all observations
 df <- data.frame(subject,activity = activity[activityObservation$code,2], featureObservations[,featureMeanStd])
 
 # write a file with the tidy data set
-write.table(df,"tidy.txt",row.name=FALSE) 
+#write.table(df,"tidy.txt",row.name=FALSE) 
+require(reshape2)
+
+melted <- melt(df, id.vars = c("subject","activity"))
